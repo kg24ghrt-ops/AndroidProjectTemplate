c7726f3521e644801afc3417627d226c14a5285c
akliuxingyuan <akliuxingyuan@gmail.com>
2025-04-05 20:31:34 +0800

    fix toolbar style

--- automation/iceraven/patch_android_components.sh
+++ automation/iceraven/patch_android_components.sh
@@ -15,3 +15,4 @@ sed -i "s#gleanPythonEnvDir#// gleanPythonEnvDir#g" android-components/**/*.grad
 
 git -C android-components apply < automation/iceraven/patches/top_sites_no_most_visted_sites.patch
 git -C android-components apply < automation/iceraven/patches/suggestions_increase_number.patch
+git -C android-components apply < automation/iceraven/patches/toolbar.patch
