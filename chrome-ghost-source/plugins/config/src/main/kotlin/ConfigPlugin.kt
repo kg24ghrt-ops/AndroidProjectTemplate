dd741651a69913b57f3ff101cbe1aae3aca49330
Ko Ko <your-email@example.com>
2026-02-07 10:17:01 +0630

    CHROME-GHOST: Restored config and focus-dependencies plugin skeletons

---
+++ chrome-ghost-source/plugins/config/src/main/kotlin/ConfigPlugin.kt
@@ -0,0 +1,5 @@
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+class ConfigPlugin : Plugin<Project> {
+    override fun apply(project: Project) {}
+}
---
+++ chrome-ghost-source/plugins/focus-dependencies/src/main/kotlin/FocusDependenciesPlugin.kt
@@ -0,0 +1,5 @@
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+class FocusDependenciesPlugin : Plugin<Project> {
+    override fun apply(project: Project) {}
+}
