a58f5171ac828ac51d7d8d9f1cfb2e5b8c034884
Ko Ko <your-email@example.com>
2026-02-07 10:14:50 +0630

    CHROME-GHOST: Reconstructed DependenciesPlugin skeleton

---
+++ chrome-ghost-source/plugins/dependencies/src/main/kotlin/DependenciesPlugin.kt
@@ -0,0 +1,8 @@
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class DependenciesPlugin : Plugin<Project> {
+    override fun apply(project: Project) {
+        // This is a stub to let the build pass initialization
+    }
+}
